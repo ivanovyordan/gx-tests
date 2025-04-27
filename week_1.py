96dce8e73193c8c392e8d2c12517906ff51494dd
Yordan Ivanov <mail@ivanovyordan.com>
2025-04-27 11:48:03 +0300

    Add Simplified Week 1 setup

--- week_1.py
+++ week_1.py
@@ -1,28 +1,12 @@
 import great_expectations as gx
 
-# Initialize Great Expectations context
 context = gx.get_context()
 
-# Define the source folder and data source name
-source_folder = "./data"
-data_source_name = "orders_data_source"
-data_source = context.data_sources.add_pandas_filesystem(
-    name=data_source_name,
-    base_directory=source_folder
-)
+file_path = "data/orders.csv"
+batch = context.data_sources.pandas_default.read_csv(file_path)
 
-# Define the asset name and add the CSV file as an asset
-asset_name = "orders_csv_file"
-file_data_asset = data_source.add_csv_asset(name=asset_name)
+expectation = gx.expectations.ExpectColumnValuesToNotBeNull(column="Customer_ID")
 
-# Define the batch definition name and path
-batch_definition_name = "orders.csv"
-batch_definition_path = "orders.csv"
-batch_definition = file_data_asset.add_batch_definition_path(
-    name=batch_definition_name,
-    path=batch_definition_path
-)
+validation_results = batch.validate(expectation)
 
-# Get the batch
-batch = batch_definition.get_batch()
-print(batch.head())
+print(validation_results)
