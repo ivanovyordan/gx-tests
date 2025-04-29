8ec7ec3f6ec4d6371418ec2db850b93515501cf7
Yordan Ivanov <mail@ivanovyordan.com>
2025-04-29 22:53:04 +0300

    Move Python files and prepare for dbt

--- python/week_1.py
+++ python/week_1.py
@@ -1,9 +1,13 @@
+import os
 import great_expectations as gx
 
 context = gx.get_context()
 
-file_path = "data/orders.csv"
-batch = context.data_sources.pandas_default.read_csv(file_path)
+batch = context.data_sources.pandas_default.read_csv(os.path.join(
+    os.path.dirname(__file__),
+    "..",
+    "data", "orders.csv")
+)
 
 expectation = gx.expectations.ExpectColumnValuesToNotBeNull(column="Customer_ID")
 
